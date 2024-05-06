5868ce6db12dc1c489cde27181c8cbc69b158a4d
Flávio Rocha <flavio-10_28@hotmail.com>
2024-05-06 11:52:55 -0300

    first commit

---
+++ src/test/java/br/com/codeup/transfer/TransferSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codeup.transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
