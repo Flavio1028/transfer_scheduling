90ec2021975312865cfa6a051689f3613d1b895c
Flávio Rocha <flavio-10_28@hotmail.com>
2024-05-07 10:22:14 -0300

    Criação do calculo da taxa

--- src/main/java/br/com/codeup/transfer/service/impl/SchedulingServiceImpl.java
+++ src/main/java/br/com/codeup/transfer/service/impl/SchedulingServiceImpl.java
@@ -16,19 +16,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 @Service
 @Validated
 public class SchedulingServiceImpl implements SchedulingService {
 
-    @Autowired
-    private SchedulingRepository schedulingRepository;
+    private final SchedulingRepository schedulingRepository;
+
+    private final AccountRepository accountRepository;
+
+    private final ModelMapper modelMapper;
 
     @Autowired
-    private AccountRepository accountRepository;
-
-    @Autowired
-    private ModelMapper modelMapper;
+    public SchedulingServiceImpl(AccountRepository accountRepository, ModelMapper modelMapper, SchedulingRepository schedulingRepository) {
+        this.accountRepository = accountRepository;
+        this.modelMapper = modelMapper;
+        this.schedulingRepository = schedulingRepository;
+    }
 
     @Override
     public SuccessDTO scheduleOperation(@Valid ScheduleDTO request) {
@@ -37,22 +45,55 @@ public class SchedulingServiceImpl implements SchedulingService {
         Account originAccount = this.getUserById(request.getOriginAccount(), ErrorMessageEnum.SOURCE_ACCOUNT_NOT_FOUND);
         Account destinationAccount = this.getUserById(request.getDestinationAccount(), ErrorMessageEnum.DESTINATION_COUNT_NOT_FOUND);
 
+        // Valida a data de trasferencia
+        this.validateDataTransfer(request.getDataTransfer());
+
         Scheduling scheduling = this.modelMapper.map(request, Scheduling.class);
         scheduling.setOriginAccount(originAccount);
         scheduling.setDestinationAccount(destinationAccount);
-
-        scheduling.setTax(BigDecimal.ZERO);
+        scheduling.setTax(this.calculateRate(request.getDataTransfer(), request.getTransferValue()));
 
         this.schedulingRepository.save(scheduling);
 
-        return SuccessDTO.builder()
-                .message("Agendamento realizado com sucesso.")
-                .build();
+        return SuccessDTO.builder().message("Agendamento realizado com sucesso.").build();
     }
 
     private Account getUserById(String accountNumber, ErrorMessageEnum errorMessage) {
-        return this.accountRepository.findByAccountNumber(accountNumber)
-                .orElseThrow(() -> new ValidationErrorException(errorMessage));
+        return this.accountRepository.findByAccountNumber(accountNumber).orElseThrow(() -> new ValidationErrorException(errorMessage));
+    }
+
+    private void validateDataTransfer(LocalDateTime date) {
+        LocalDateTime now = LocalDateTime.of(LocalDate.now(), LocalTime.of(0, 0));
+        if (now.isAfter(date)) {
+            throw new ValidationErrorException(ErrorMessageEnum.iNVALID_DATA_TRANSFER);
+        }
+    }
+
+    private BigDecimal calculateRate(LocalDateTime dataTransfer, BigDecimal transferValue) {
+
+        BigDecimal calculatedRate;
+
+        // Calcula a data para do agendamento
+        LocalDateTime today = LocalDateTime.of(LocalDate.now(), LocalTime.of(0, 0));
+        long days = today.until(dataTransfer, ChronoUnit.DAYS);
+
+        if (days == 0) {
+            calculatedRate = transferValue.multiply(BigDecimal.valueOf(0.025));
+        } else if (days <= 10) {
+            calculatedRate = transferValue.multiply(BigDecimal.valueOf(0));
+        } else if (days <= 20) {
+            calculatedRate = transferValue.multiply(BigDecimal.valueOf(0.082));
+        } else if (days <= 30) {
+            calculatedRate = transferValue.multiply(BigDecimal.valueOf(0.069));
+        } else if (days <= 40) {
+            calculatedRate = transferValue.multiply(BigDecimal.valueOf(0.047));
+        } else if (days <= 50) {
+            calculatedRate = transferValue.multiply(BigDecimal.valueOf(0.017));
+        } else {
+            throw new ValidationErrorException(ErrorMessageEnum.INVALID_RATE);
+        }
+
+        return calculatedRate;
     }
 
 }
--- src/main/java/br/com/codeup/transfer/util/enums/ErrorMessageEnum.java
+++ src/main/java/br/com/codeup/transfer/util/enums/ErrorMessageEnum.java
@@ -12,7 +12,9 @@ public enum ErrorMessageEnum {
     USER_NOT_FOUND("Usuário não encontrado."),
     DOCUMENT_ALREADY_REGISTERED("Documento já cadastrado"),
     SOURCE_ACCOUNT_NOT_FOUND("Conta de origem não localizada."),
-    DESTINATION_COUNT_NOT_FOUND("Conta de destino não localizada.");
+    DESTINATION_COUNT_NOT_FOUND("Conta de destino não localizada."),
+    iNVALID_DATA_TRANSFER("A data de trasferência não pode ser anterior ao dia atual."),
+    INVALID_RATE("Taxa não aplicavel para este tipo de agendamento.");
 
     private final String message;
 
